112cb074fd9493b29a40509beb940c94154bdecf
Luke Eltiste <luke343279@gmail.com>
2021-09-20 13:26:22 -0700

    Add project files.

---
+++ src/Get.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "HTTPRequest.hpp"
+
+int main()
+{
+  try
+  {
+    // you can pass http::InternetProtocol::V6 to Request to make an IPv6 request
+    http::Request request { "http://dweet.io/get/latest/dweet/for/frazzertest" };
+
+    // send a get request
+    const auto response = request.send( "GET" );
+    std::cout << "Description: " << response.description << '\n';    // print the description
+    std::cout << "Headers: ";
+    for( std::string s : response.headers ) std::cout << '\t' << s << '\n';                         // print the headers
+    std::cout << "Body: " << std::string { response.body.begin(), response.body.end() } << '\n';    // print the body
+  }
+  catch( const std::exception & e )
+  {
+    std::cerr << "Request failed, error: " << e.what() << '\n';
+  }
+}
---
+++ src/Post.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include "HTTPRequest.hpp"
+
+int main()
+{
+  try
+  {
+    http::Request request { "http://dweet.io/dweet/for/frazzertest" };
+    // send a post request
+    const auto response = request.send( "POST", "{\"foo\":\"bar\"}", { "Content-Type: application/json" } );
+    std::cout << "Description: " << response.description << '\n';    // print the description
+    std::cout << "Headers: ";
+    for( std::string s : response.headers ) std::cout << '\t' << s << '\n';                         // print the headers
+    std::cout << "Body: " << std::string { response.body.begin(), response.body.end() } << '\n';    // print the body
+  }
+  catch( const std::exception & e )
+  {
+    std::cerr << "Request failed, error: " << e.what() << '\n';
+  }
+}
